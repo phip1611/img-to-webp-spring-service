f62e935630e99d0145ad7e388ff52bf2bd7de78e
Philipp Schuster <phip1611@gmail.com>
2023-10-03 17:20:27 +0200

    modernize dockerfile

--- build-docker.sh
+++ build-docker.sh
@@ -2,6 +2,7 @@
 
 set -e
 mvn clean install
+# mvn -DskipTests=true clean install
 cd ./img-to-webp-service
 docker build --build-arg JAR_FILE=target/*.jar -t phip1611/img-to-webp-service .
 echo "built Docker image 'phip1611/img-to-webp-service'"
