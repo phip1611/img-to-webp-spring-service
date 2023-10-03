63d2ae83059ace6823188d5cad68facc1f29906b
Philipp Schuster <phip1611@gmail.com>
2023-10-03 17:25:14 +0200

    ci: build docker image

--- build-docker.sh
+++ build-docker.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-set -e
-mvn clean install
-# mvn -DskipTests=true clean install
+set -euo pipefail
+
+# Build the JAR file.
+mvn clean package -DskipTests=true
+
 cd ./img-to-webp-service
-docker build --build-arg JAR_FILE=target/*.jar -t phip1611/img-to-webp-service .
+
+echo "building Docker image 'phip1611/img-to-webp-service'"
+docker build -t phip1611/img-to-webp-service .
 echo "built Docker image 'phip1611/img-to-webp-service'"
