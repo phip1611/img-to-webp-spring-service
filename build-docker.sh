8a529595682e88421cc66a39ce18cae05fcad0fb
Philipp Schuster <phip1611@gmail.com>
2023-10-03 17:10:43 +0200

    build: fix docker build

--- build-docker.sh
+++ build-docker.sh
@@ -1,9 +1,7 @@
-#!/bin/zsh
+#!/usr/bin/env bash
 
 set -e
 mvn clean install
 cd ./img-to-webp-service
-# from: https://spring.io/guides/gs/spring-boot-docker/
-mkdir -p target/dependency && (cd target/dependency; jar -xf ../*.jar)
-docker build -t phip1611/img-to-webp-service .
+docker build --build-arg JAR_FILE=target/*.jar -t phip1611/img-to-webp-service .
 echo "built Docker image 'phip1611/img-to-webp-service'"
