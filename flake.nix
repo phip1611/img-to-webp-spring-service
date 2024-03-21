990aefe1e161f3fc369b1be8a745f010ab7fc283
Philipp Schuster <phip1611@gmail.com>
2024-03-21 18:07:46 +0100

    flake: update nixpkgs

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
 
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
   };
 
   outputs = inputs@{ flake-parts, nixpkgs, ... }:
