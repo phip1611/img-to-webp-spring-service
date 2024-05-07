ee6c5273944d33d706e019123f0cc833abb03059
Philipp Schuster <phip1611@gmail.com>
2024-05-07 22:50:41 +0200

    js: add matomo tracking code
    This adds a GDPR friendly Matomo tracking code. Actually, no cookies, thus,
    tracking happens, but just simple user analysis with anonymized statistics.

--- img-to-webp-service/src/main/resources/static/res/js/custom.js
+++ img-to-webp-service/src/main/resources/static/res/js/custom.js
@@ -21,3 +21,21 @@ window.addEventListener('load', () => {
         uploadButtonEle.disabled = !event.target.checked;
     });
 });
+
+<!-- Matomo -->
+    var _paq = window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['disableCookies']);
+    _paq.push(['enableHeartBeatTimer']);
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+    var u="https://yikes.phip1611.de/";
+    // Rewritten to matomo.php on server.
+    _paq.push(['setTrackerUrl', u+'yikes.php']);
+    _paq.push(['setSiteId', '11']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    // Rewritten to matomo.js on server.
+    g.async=true; g.src=u+'yikes.js'; s.parentNode.insertBefore(g,s);
+})();
+<!-- End Matomo Code -->
