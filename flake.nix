bc297a1df733150fd584b545194c0a67a76a6883
Philipp Schuster <phip1611@gmail.com>
2026-05-27 09:40:38 +0200

    nix: fix warnings for nixos-26.05 (no nested list)

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   description = "Image To WebP Spring Service";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
   outputs =
